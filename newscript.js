076754331b40da4382db9f2f76f483ec9ab9ebb5
jmraffi84 <jmraffi@gmail.com>
2023-01-13 23:34:14 +0530

    first commit

---
+++ newscript.js
@@ -0,0 +1,49 @@
+// Question 1:
+// iterate all over the loops ( for, for in, for Of, forEach)
+
+// for loop 1:
+// let a = [1,2,3,4,5]
+
+// for(let i=0; i<a.length; i++){
+//     console.log(a[i])
+
+// }
+// output : 1 2 3 4 5
+
+// <------------End of For------------------------------>
+
+
+// for in 2:
+
+// let a = [1,2,3,4,5]
+
+// for(let i in a){
+//     console.log(a[i])
+
+// }
+// output : 1 2 3 4 5
+
+// <------------End of For in------------------------------>
+
+// for of 3:
+
+// let a = [1,2,3,4,5]
+
+// for(let i of a){
+//     console.log(i)
+
+// }
+// output : 1 2 3 4 5
+
+// <------------End of For of------------------------------>
+
+// for Each 4:
+
+// let course = ["guvi", "zen", "html", "css", "javascript"]
+// course.forEach(element =>{return element})
+// console.log(course)
+
+// output:  ['guvi', 'zen', 'html', 'css', 'javascript']
+
+// <------------End of ForEach------------------------------>
+
---
+++ resume.js
@@ -0,0 +1,77 @@
+// {
+//     "resume":{
+//         "name"          :   "Your Name",
+//         "photo"         :   "your photo",
+//         "phone number"  :   "your Phone Number",
+//         "Email"         :   "Your Email",
+//         "qualification" :   "Your latest quatification",
+
+//     "location":{
+//         "address"       :   "Your Full Address",
+//         "Postal Code"   :   "Your Postal Code",
+//         "Country Origin":   "Your Country",
+//         "City"          :   "Your City",
+//         "State"         :   "Your State",
+//         },
+//     "profile":[{
+//         "network"       :   "Your Linkedin Media Network",
+//         "username"      :   "Your Network User Name",
+//         "path"          :   "Your Network Url Path",
+//     }]    
+
+
+//     },
+//     "work":[
+//         {
+//         "company"       : "Your employer name",
+//         "position"      : "Your job title",
+//         "website"       : "The URL for the employer's website",
+//         "startDate"     : "Your start date, in YYYY-MM-DD format",
+//         "endDate"       : "Your end date, in YYY-MM-DD format (leave blank for a current position)",
+        
+//         "acheivements": [
+//             "List out your past acheivements."
+//           ]
+//     }
+//     ],
+//     "education":[
+//         {
+//           "institution" : "Your school name",
+//           "area"        : "Your area of study or degree earned",
+//           "studyType"   : "",
+//           "startYear"   : "Your start date, in YYYY-MM-DD format",
+//           "CompletedYear": "Your completion date, in YYYY-MM-DD format",
+         
+//           "courses": [
+//             ""
+//           ]
+//         }
+//       ],
+//     "skills":[
+//         {
+//           "name"        : "A category of job skills (e.g. 'Programming Languages')",
+//           "level"       : "",
+
+//         "keywords": [
+//             "Keywords under this category (e.g. 'Java', 'C++', etc)"
+//           ]
+//         }
+//       ],
+//     "languages":[
+//         {
+//           "language"    : "Language name",
+//           "fluency"     : "Your language fluency"
+//         }
+//       ],
+//     "interests":[
+//         {
+//           "name"        : "A category of interests (e.g. 'Sports')",
+
+//     "keywords":[
+//             "Keywords under this category (e.g. 'Cricket', 'Football', 'Golf')"
+//           ]
+//         }
+//       ]
+    
+
+// }
